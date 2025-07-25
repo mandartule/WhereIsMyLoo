57915effc9fb78e7521f8dc56d01792ce2dfc3f9
Mandar Tule <mandartule5@gmail.com>
2025-07-25 21:47:42 +0530

    view engine set to ejs

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 
 app.get('/', (req, res) => {
-    res.send('Hello there !')
+    //res.send('Hello there !')
+    res.render('temp');
 })
 
 app.listen(3030, () => {
